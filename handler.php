37fd8996dddde017fb942fcf2c6738f0be431de4
InduManimaran <indu.manimaran@gmail.com>
2018-05-12 12:26:31 -0700

    Designed main page

--- handler.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_POST['submit'])){
-header("Location: http://www.example.com/page.php");
-exit;
-}
-?>
--- table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Expenses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL,
-  `value` float NOT NULL,
-  `reason` varchar(150) NOT NULL,
-  `vat` float NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
